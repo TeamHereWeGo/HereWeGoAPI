c869efee3cb0b10b00a6114f4e2a4d321911fde0
HYEONSUK KIM <sukrrard97@gmail.com>
2023-08-05 10:09:37 +0900

    init project

---
+++ src/main/java/com/hwgapp/hwgapi/HwgapiApplication.java
@@ -0,0 +1,13 @@
+package com.hwgapp.hwgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwgapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwgapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hwgapp/hwgapi/HwgapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwgapp.hwgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwgapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
